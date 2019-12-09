215da7024b6a3b12e43311978daa74fcc79bda63
Hưng <kirabboytt@gmail.com>
2019-12-09 17:19:53 +0700

    Revert "Initial commit"
    This reverts commit 2eca9f53060dcab1f2347328fd89d643ed727750.

--- src/main/java/com/devpro/spring/repository/UserRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devpro.spring.repository;
-
-import com.devpro.spring.model.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Long>{
-
-}
--- src/main/java/com/devpro/spring/service/OrderFoodService.java
+++
@@ -1,8 +0,0 @@
-package com.devpro.spring.service;
-
-import com.devpro.spring.model.OrderFood;
-
-public interface OrderFoodService {
-
-	void addOrderFood(OrderFood orderFood);
-}
